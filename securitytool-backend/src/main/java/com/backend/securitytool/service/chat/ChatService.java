01e2d5bd5a91c5dd4911d9787cafebcebc686002
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2025-06-28 11:41:15 +0700

    minor change

--- securitytool-backend/src/main/java/com/backend/securitytool/service/chat/ChatService.java
+++ securitytool-backend/src/main/java/com/backend/securitytool/service/chat/ChatService.java
@@ -1,10 +1,9 @@
 package com.backend.securitytool.service.chat;
 
 import com.backend.securitytool.model.dto.request.ChatRequestDTO;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface ChatService {
-    public String chatWithAI(ChatRequestDTO message);
+    String chatWithAI(ChatRequestDTO message);
 }
 
